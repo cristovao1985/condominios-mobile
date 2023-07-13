73498c3487b4e5910a01f069847457480ff11784
Cristovao Jandson <cristovaojandson@gmail.com>
2023-07-13 17:28:19 -0300

    feat: setando push para rota home

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,11 @@
 
 <script>
 import { defineComponent } from "vue";
-
+import router from "./router";
 export default defineComponent({
   name: "App",
+  created() {
+    this.$router.push({ name: "home" });
+  },
 });
 </script>
